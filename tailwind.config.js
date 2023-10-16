bb6022d5baa5f58d68177484b56359fbc3faf0b8
Bethan Helen <bethanhelen8@gmail.com>
2023-10-16 02:30:35 +0100

    images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans:['Nunito Sans', sans-serif],
+        sans:["Nunito Sans", "sans-serif"],
+    },
+    backgroundImage: {
+      'home': "url('/img/Home.png')",
+      'about': "url('/img/About.png')",
+      'programs': "url('/img/Programs.png')",
+      'contact': "url('/img/Contact Us.png')",
+    },
+    fontWeight:{
+    sm:["200"],
+    lg:["300"],
+    xl:["400"],
+    "2xl":["500"],
+    "3xl":["600"],
+    "4xl":["700"],
+    "5xl":["800"],
     },
   },
+},
   plugins: [],
-  };
+};
